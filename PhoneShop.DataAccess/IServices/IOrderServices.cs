e27cac7e04952aec2807e71c972368c017a3a768
touanu <taongo10x@gmail.com>
2024-07-06 16:27:46 +0700

    Thêm IOrderServices

---
+++ PhoneShop.DataAccess/IServices/IOrderServices.cs
@@ -0,0 +1,11 @@
+﻿using PhoneShop.DataAccess.DTO;
+
+namespace PhoneShop.DataAccess.IServices
+{
+    public interface IOrderServices
+    {
+        Task<ReturnData> Order_Add(OrderRequestData requestData);
+        Task<ReturnData> Order_Delete(string orderId);
+        Task<ReturnData> Order_Update(OrderRequestData requestData);
+    }
+}
