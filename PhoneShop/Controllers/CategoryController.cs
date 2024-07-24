0c43ee96635f20937eb14f2a224690d4ea8e372d
Nhật Long <longnhat678@outlook.com>
2024-07-24 23:06:50 +0700

    Update CategoryServices

---
+++ PhoneShop/Controllers/CategoryController.cs
@@ -0,0 +1,83 @@
+﻿using CommonLibs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PhoneShop.DataAccess.DTO;
+
+namespace PhoneShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        readonly IConfiguration _configuration;
+        public CategoryController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddCategory()
+        {
+            var token = Request.Cookies["MY_JWT_TOKEN"] != null ? Request.Cookies["MY_JWT_TOKEN"].ToString() : "";
+            if (string.IsNullOrEmpty(token))
+            {
+                return Redirect("/Account/Login");
+            }
+            return View();
+        }
+        public async Task<JsonResult> AddCategorys(CategoryRequestData requestData)
+        {
+            var model = new GetCategoryReturnData();
+            var messageFromServer = string.Empty;
+            try
+            {
+                var token = Request.Cookies["MY_JWT_TOKEN"] != null ? Request.Cookies["MY_JWT_TOKEN"].ToString() : "";
+                if (string.IsNullOrEmpty(token))
+                {
+                    messageFromServer = "Vui lòng đăng nhập";
+                    return Json(messageFromServer);
+                }
+                if (requestData == null
+                    || string.IsNullOrEmpty(requestData.CategoryName))
+                {
+                    model.ReturnCode = -1;
+                    model.ReturnMsg = "Dữ liệu không được trống";
+                    return Json(model);
+                }
+
+
+                // Bước 1.1 : khai báo API URL
+
+                var baseurl = _configuration["API_URL:URL"] ?? "";
+                var url = "api/Category/AddCategory";
+
+                // bƯỚC 1.2: tạo json data ( object sang JSON)
+                var jsonData = JsonConvert.SerializeObject(requestData);
+
+                // Bước 1.3 : gọi httpclient bên common để post lên api
+                var result = await HttpHelper.HttpSenPostWithToken(baseurl, url, jsonData,token);
+
+                // Bước 1.4: nhận dữ liệu về 
+                var imageName = "";
+                if (string.IsNullOrEmpty(result))
+                {
+                    model.ReturnCode = -2;
+                    model.ReturnMsg = "Lỗi";
+                    return Json(model);
+                }
+                var rs = JsonConvert.DeserializeObject<ReturnData>(result);
+                model.ReturnCode = rs.ReturnCode;
+                model.ReturnMsg = rs.ReturnMsg;
+                return Json(model);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return Json(model);
+        }
+    }
+}
--- PhoneShopAPI/Controllers/CategoryController.cs
+++ PhoneShopAPI/Controllers/CategoryController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CommonLibs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using PhoneShop.DataAccess.DTO;
 using PhoneShop.DataAccess.UnitOfWork;
+using PhoneShopAPI.Models;
 
 namespace PhoneShopAPI.Controllers
 {
@@ -30,6 +33,47 @@ namespace PhoneShopAPI.Controllers
                 returnData.ReturnMsg = "Dữ liệu đầu vào không hợp lệ!";
                 return Ok(returnData);
             }
+            // bước 1: gọi media để upload ảnh 
+
+            // Bước 1.1 : khai báo API URL
+
+            var baseurl = "http://localhost:5124/";
+            var url = "api/Media/Upload";
+
+            // bƯỚC 1.2: tạo json data ( object sang JSON)
+
+            // kiểm tra xem chữ ký có hợp lệ không ?
+            var SecretKey = _configuration["Sercurity:SecretKey"] ?? "";
+
+            var plantext = requestData.IconImages + SecretKey;
+
+            var Sign =Security.MD5(plantext);
+
+            var requestUpload = new UploadRequestData
+            {
+                Base64Image = requestData.IconImages,
+                Sign = Sign
+            };
+
+            var jsonData = JsonConvert.SerializeObject(requestUpload);
+
+            // Bước 1.3 : gọi httpclient bên common để post lên api
+            var result = await HttpHelper.HttpSendPost(baseurl, url, jsonData);
+
+            // Bước 1.4: nhận dữ liệu về 
+            var imageName = "";
+            if (!string.IsNullOrEmpty(result))
+            {
+                var rs = JsonConvert.DeserializeObject<ReturnData>(result);
+                if (rs != null && rs.ReturnCode > 0)
+                {
+                    imageName = rs.ReturnMsg;
+                }
+            }
+
+
+            // bước 2: có ảnh từ bước 1 rồi thì thực hiện lưu xuống db
+            requestData.IconImages = imageName;
             var response = await _unitOfWork._categoryServices.AddCategory(requestData);
             _unitOfWork.SaveChange();
             returnData.ReturnCode = response.ReturnCode;
@@ -39,7 +83,7 @@ namespace PhoneShopAPI.Controllers
         [HttpPost("GetCategory")]
         public async Task<ActionResult> GetCategory(CategoryRequestData requuestData)
         {
-            var list = await _unitOfWork._categoryServices.AddCategory(requuestData);
+            var list = await _unitOfWork._categoryServices.GetCategories(requuestData);
 
             return Ok(list);
         }
