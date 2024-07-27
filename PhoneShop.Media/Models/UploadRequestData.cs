456c9af30a70949097e2958b763a01113a7ad644
Nhật Long <longnhat678@outlook.com>
2024-07-27 22:45:26 +0700

    update theo media Server

--- PhoneShop.Media/Models/UploadRequestData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PhoneShop.Media.Models
-{
-    public class UploadRequestData
-    {
-        public string? Base64Image { get; set; }
-        public string? Sign {  get; set; }
-    }
-}
